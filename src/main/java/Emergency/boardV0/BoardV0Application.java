86ec1e5d25ef33951ff2e8abf04d9300b8f5b88a
jngbn <[user email]>
2025-09-04 13:01:15 +0900

    Initial commit from IntelliJ

---
+++ src/main/java/Emergency/boardV0/BoardV0Application.java
@@ -0,0 +1,13 @@
+package Emergency.boardV0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardV0Application.class, args);
+	}
+
+}
